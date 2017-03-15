11b559c30e4984099d91548c92670e5cddb8b5f6
Ivy Chang <ivycwq@icloud.com>
2017-03-15 14:09:35 -0700

    initial setup

---
+++ LibraryItem.java
@@ -0,0 +1,17 @@
+public class LibraryItem {
+  private int id;
+  private String description;
+  private static int next_ID = 0;
+
+  public LibraryItem() {
+    id = ++next_ID;
+  }
+
+  public void setDescription(String s) {
+    description = s;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+}
